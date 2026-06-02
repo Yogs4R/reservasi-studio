b7e38e44a2c4b0442e9d8916b3ffbd095def3e12
ZakiLearn <01abyanzaki@gmail.com>
2026-06-02 21:11:33 +0700

    menambahkan page user admin

---
+++ modules/admin/user/index.php
@@ -0,0 +1,89 @@
+<?php
+require '../../../config/koneksi.php';
+$query = 'SELECT * FROM user ';
+
+if (!empty($_GET['search'])) {
+    $search = mysqli_real_escape_string($conn, $_GET['search']);
+    $query .= " WHERE nama LIKE '%$search%'";
+    $query .= " OR email LIKE '%$search%'";
+    $query .= " OR no_hp LIKE '%$search%'";
+}
+
+$query .= ' ORDER BY id_user;';
+$hasil = mysqli_query($conn, $query);
+?>
+<?php include '../../../includes/header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container pt-5 pb-4 mt-5">
+        <div class="d-flex justify-content-between align-items-end border-bottom pb-3 mb-4">
+
+            <div>
+                <h1 class="fw-bold mb-1">Admin Page User</h1>
+                <p class="text-muted mb-0">
+                    Browse available equipment and creative assets.
+                </p>
+            </div>
+        </div>
+    </div>
+    <form method="GET" class="row g-2 mb-4">
+        <div class="col-md-10">
+            <input type="text" name="search" class="form-control" placeholder="Search User" value="<?= $_GET[
+                'search'
+            ] ?? '' ?>">
+        </div>
+        <div class="col-md-2">
+            <button type="submit" class="btn btn-dark w-100">Search</button>
+        </div>
+    </form>
+    <div class="row g-2 mb-4">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Nama</th>
+                    <th>Email</th>
+                    <th>No HP</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                    <?php
+                    $no = 1;
+                    while ($data = mysqli_fetch_array($hasil)) { ?> 
+                            <tr>
+                                <td><?php echo $no; ?></td>
+                                <td><?php echo $data['nama']; ?></td>
+                                <td><?php echo $data['email']; ?></td>
+                                <td><?php echo $data['no_hp']; ?></td>
+                                <td>
+                                    <a href="./update.php?id_user=<?= $data[
+                                        'id_user'
+                                    ] ?>" class="btn btn-warning btn-sm" style="padding-bottom: 8px;">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                            <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
+                                        </svg>
+                                    </a>
+                                    <a href="./delete.php?id_user=<?= $data[
+                                        'id_user'
+                                    ] ?>" class="btn btn-danger btn-sm" style="padding-bottom: 8px;">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                                          <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
+                                        </svg>
+                                    </a>
+                                </td>
+                            </tr>
+                        <?php $no++;}
+                    ?>
+            </tbody>
+        </table>
+    </div>    
+</body>
+</html>
+<?php include '../../../includes/footer.php'; ?>
